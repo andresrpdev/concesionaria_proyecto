a7bbb3005755a902b149d80b4df2ce3aa13b4108
andresrpdev <andresromanutti53@gmail.com>
2025-03-09 17:22:39 -0300

    vercel add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   vite: {
     assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.webp'],
   },
-  output: "server"
+
+  output: "server",
+  adapter: vercel()
 });
