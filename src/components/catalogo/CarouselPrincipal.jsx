1cfc3f1995253acc9a01a9b187764a4ae195841a
andresrpdev <2416872@ucc.edu.ar>
2025-03-09 21:20:23 -0300

    fixing img again

--- src/components/catalogo/CarouselPrincipal.jsx
+++ src/components/catalogo/CarouselPrincipal.jsx
@@ -1,45 +1,46 @@
-import { useEffect , useState} from 'react';
+import { useEffect, useState } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import "../../styles/EmblaCarousel.css";
 
 // Importar las imágenes
-import bmwImg from '../../assets/carusel/bmw.jpg';
-import f150Img from '../../assets/carusel/f150r.jpg';
-import mercedesImg from '../../assets/carusel/mercedes.jpg';
-import jeepImg from '../../assets/carusel/jeep.jpg';
-import teslaImg from '../../assets/carusel/tesla.jpg';
+import bmwImg from '/carusel/bmw.jpg';
+import f150Img from '/carusel/f150r.jpg';
+import mercedesImg from '/carusel/mercedes.jpg';
+import jeepImg from '/carusel/jeep.jpg';
+import teslaImg from '/carusel/tesla.jpg';
 
 const vehicles = [
-    { img: bmwImg, title: "BMW Serie 2", subtitle: "Confort y rendimiento en su máxima expresión" },
-    { img: f150Img, title: "Ford F150", subtitle: "La camioneta robusta para cualquier desafío" },
-    { img: mercedesImg, title: "Mercedes-Benz Clase E", subtitle: "Elegancia y potencia combinadas" },
-    { img: jeepImg, title: "Jeep Wrangler", subtitle: "El rey del todoterreno" },
-    { img: teslaImg, title: "Tesla Model S", subtitle: "El futuro eléctrico del rendimiento" },
+  { img: bmwImg, title: "BMW Serie 2", subtitle: "Confort y rendimiento en su máxima expresión" },
+  { img: f150Img, title: "Ford F150", subtitle: "La camioneta robusta para cualquier desafío" },
+  { img: mercedesImg, title: "Mercedes-Benz Clase E", subtitle: "Elegancia y potencia combinadas" },
+  { img: jeepImg, title: "Jeep Wrangler", subtitle: "El rey del todoterreno" },
+  { img: teslaImg, title: "Tesla Model S", subtitle: "El futuro eléctrico del rendimiento" },
 ];
+
 export default function EmblaCarousel() {
-    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 3500 })]);
-    const [randomVehicles, setRandomVehicles] = useState([]);
-  
-    useEffect(() => {
-      // Barajar los vehículos y seleccionar los primeros tres
-      const shuffledVehicles = vehicles.sort(() => 0.5 - Math.random());
-      setRandomVehicles(shuffledVehicles.slice(0, 3));
-    }, []);
-  
-    return (
-      <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-          {randomVehicles.map((vehicle, index) => (
-            <div className="embla__slide" key={index}>
-              <img src={vehicle.img} alt={vehicle.title} />
-              <div className="embla__text">
-                <h2 className="embla__title">{vehicle.title}</h2>
-                <p className="embla__subtitle">{vehicle.subtitle}</p>
-              </div>
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 3500 })]);
+  const [randomVehicles, setRandomVehicles] = useState([]);
+
+  useEffect(() => {
+    // Barajar los vehículos y seleccionar los primeros tres
+    const shuffledVehicles = vehicles.sort(() => 0.5 - Math.random());
+    setRandomVehicles(shuffledVehicles.slice(0, 3));
+  }, []);
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {randomVehicles.map((vehicle, index) => (
+          <div className="embla__slide" key={index}>
+            <img src={vehicle.img} alt={vehicle.title} />
+            <div className="embla__text">
+              <h2 className="embla__title">{vehicle.title}</h2>
+              <p className="embla__subtitle">{vehicle.subtitle}</p>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
